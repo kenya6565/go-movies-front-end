5e962da7db8214b8775134d92c6600176c35efa6
kenya6565 <ken.australia0605@gmail.com>
2023-01-18 02:12:03 +0900

    create Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h2>Find a movie to watch tonight!</h2>
+    </div>
+  );
+};
+
+export default Home;
