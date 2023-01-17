6c1bdfe142943851e3703ef9c6b912c33eda937a
kenya6565 <ken.australia0605@gmail.com>
2023-01-18 02:02:04 +0900

    introduce Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
   return (
-    <div>
-      Go Watch a Movie!
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
