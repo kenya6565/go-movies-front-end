9cdf3d4b5305c3b834c9dd7b9ce8f27dcf8f9f2e
kenya6565 <ken.australia0605@gmail.com>
2023-01-19 02:54:09 +0900

    manage Movies component in router

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./components/ErrorPage";
 import Home from "./components/Home";
+import Movies from "./components/Movies";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+    ],
   },
 ]);
 
