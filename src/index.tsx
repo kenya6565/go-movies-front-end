6d263f3aee7704fbc49f5c86a9f42c6df162781a
kenya6565 <ken.australia0605@gmail.com>
2023-01-19 02:46:21 +0900

    create ErrorPage.tsx, using useRouteError

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />
   },
 ]);
 
