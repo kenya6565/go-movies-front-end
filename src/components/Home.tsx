6bb7ebe99a5701ec31260e91aa549d9a520affb8
kenya6565 <ken.australia0605@gmail.com>
2023-01-21 17:13:59 +0900

    use Link to img

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Ticket from "./../images/movie_tickets.jpg";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="text-center">
       <h2>Find a movie to watch tonight!</h2>
       <hr />
-      <img src={Ticket} alt="movie tickets" />
+      <Link to="/movies">
+        <img src={Ticket} alt="movie tickets" />
+      </Link>
     </div>
   );
 };
